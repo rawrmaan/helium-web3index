b0532638dc6cc65a9dfc4cb51c23e3b9f75650d6
Arman Dezfuli-Arjomandi <rawrmaan@gmail.com>
2023-06-06 20:45:41 -0400

    Dynamic port

--- server.ts
+++ server.ts
@@ -22,8 +22,9 @@ app.get('/', (req, res) => {
   res.json(report)
 })
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000')
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
 })
 
 // Cache report on server start and every 8 hours
